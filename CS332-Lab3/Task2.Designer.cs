7733e643a30dc1642b1fd1711ff1c58f109ed724
Lusparon Tyzykhyan <lusparon99@bk.ru>
2020-09-29 11:19:46 +0300

    Task 2

--- CS332-Lab3/Task2.Designer.cs
+++ CS332-Lab3/Task2.Designer.cs
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(43, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(704, 313);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(474, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Начать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Task2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Task2";
             this.Text = "Task2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- CS332-Lab3/Task2.cs
+++ CS332-Lab3/Task2.cs
@@ -15,6 +15,114 @@ namespace CS332_Lab3
         public Task2()
         {
             InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
         }
+
+        Graphics g;
+        int sizePen = 1;
+        Point mouse;
+        Bitmap bmp;
+
+        //рисуем
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                g.DrawLine(new Pen(Color.Black, sizePen), mouse, e.Location);
+                mouse = e.Location;
+                pictureBox1.Refresh();
+            }
+        }
+        
+        //Начать
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var x in GetBorderPoints(bmp))
+                bmp.SetPixel(x.X, x.Y, Color.Red);
+            pictureBox1.Refresh();
+        }
+
+        //Очистить
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+                pictureBox1.Image = null;
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+        }
+
+        //Поиск начальной точки
+        private Point FindStartPoint(Bitmap sourceImage)
+        {
+            Color back_color = bmp.GetPixel(bmp.Width-1,0);
+            Color cur_color = back_color;
+            
+            for (var x = bmp.Width - 1; x >= 0  ; x--) //x
+                for (var y = 0; y < bmp.Height - 1; y++) //y
+                    if (cur_color == back_color)
+                    {
+                        cur_color = bmp.GetPixel(x,y++);
+                    }
+                    else return new Point(x,y); 
+
+            return new Point(0,0);
+        }
+
+        //Функция формирования списка граничных точек
+        private List<Point> GetBorderPoints(Bitmap bmp_img)
+        {
+            List<Point> border = new List<Point>();
+            Point cur = FindStartPoint(bmp_img);
+            border.Add(cur);
+            Point start = cur;
+            Point next = cur;
+            Color borderColor = bmp_img.GetPixel(cur.X, cur.Y);
+
+            //Будем идти против часовой стрелки и ходить внутри области
+            int dir = 8;
+            do
+            {
+                dir += dir - 1 < 0 ? 7 : -2; // поворот на 90 градусов
+                int t = dir;
+                do
+                {
+                    next = cur;
+                    switch (dir)
+                    {
+                        case 0: next.X++; break;
+                        case 1: next.X++; next.Y--; break;
+                        case 2: next.Y--;  break;
+                        case 3: next.X--; next.Y--; break;
+                        case 4: next.X--;  break;
+                        case 5: next.X--; next.Y++; break;
+                        case 6: next.Y++; break;
+                        case 7: next.X++; next.Y++; break;
+                    }
+                    //Если не нашли - останавливаемся
+                    if (next == start)
+                        break;
+                    if (bmp_img.GetPixel(next.X, next.Y) == borderColor)
+                    {
+                        //Кладем в список
+                        border.Add(next);
+                        cur = next;
+                        //cur_dir = pred_Dir;
+                        break;
+                    }
+                    dir = (dir + 1) % 8;
+                } while (dir != t);
+            } while (next != start);
+
+            return border;
+        }
+
     }
 }
